4e0daf0d881a1b853818ae3cc866e704c274bd82
Allen Zhou <Zhouball@gmail.com>
2014-09-30 14:26:09 -0400

    framework for Stock/PriceComparator testing

---
+++ src/app/StockPriceComparatorTester.java
@@ -0,0 +1,16 @@
+package app;
+
+/**
+ * Tester class for the Stock/PriceComparator subsystem
+ * @author Allen Zhou
+ *
+ */
+public class StockPriceComparatorTester {
+	
+	public static void main(String[] args) {
+		Trader t1 = new Trader("Trader1");
+		Trader t2 = new Trader("Trader2");
+		new 
+	}
+
+}
